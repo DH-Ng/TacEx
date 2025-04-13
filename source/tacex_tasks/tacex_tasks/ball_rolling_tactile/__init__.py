51fc2c25a74b34403c36a280d8a66875453a37ff
Duuuc <duce4huey@gmail.com>
2025-04-13 23:54:30 +0000

    new rew for ball rolling

--- source/tacex_tasks/tacex_tasks/ball_rolling_tactile/__init__.py
+++ source/tacex_tasks/tacex_tasks/ball_rolling_tactile/__init__.py
@@ -39,10 +39,10 @@ gym.register(
     },
 )
 # isaaclab -p ./scripts/reinforcement_learning/skrl/train.py --task TacEx-Ball-Rolling-Tactile-Base-v1 --num_envs 1000 --enable_cameras
-from .base_env_new import BallRollingEnv, BallRollingEnvCfg # need to import BallRollingEnv here, otherwise class will not be detected for entry point
+from .ball_rolling_base import BallRollingEnv, BallRollingEnvCfg # need to import BallRollingEnv here, otherwise class will not be detected for entry point
 gym.register(
     id="TacEx-Ball-Rolling-Tactile-Base-v1",
-    entry_point=f"{__name__}.base_env_new:BallRollingEnv",
+    entry_point=f"{__name__}.ball_rolling_base:BallRollingEnv",
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": BallRollingEnvCfg,
