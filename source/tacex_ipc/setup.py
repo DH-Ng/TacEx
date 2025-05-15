1ebb00eec534f22462e2df96d7291202ea5ced95
Duuuc <duce4huey@gmail.com>
2025-05-15 23:09:08 +0000

    new cmake

--- source/tacex_ipc/setup.py
+++ source/tacex_ipc/setup.py
@@ -3,7 +3,17 @@
 import os
 import toml
 
-from setuptools import setup
+import re
+import sys
+import sysconfig
+import platform
+import subprocess
+
+from distutils.version import LooseVersion
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
+
+from pathlib import Path
 
 # Obtain the extension data from the extension.toml file
 EXTENSION_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -14,6 +24,72 @@ EXTENSION_TOML_DATA = toml.load(os.path.join(EXTENSION_PATH, "config", "extensio
 INSTALL_REQUIRES = [
     # NOTE: Add dependencies
 ]
+
+'''
+Modified from https://www.benjack.io/2017/06/12/python-cpp-tests.html
+'''
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: " +
+                ", ".join(e.name for e in self.extensions))
+
+        # if platform.system() == "Windows":
+        #     cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
+        #                                  out.decode()).group(1))
+        #     if cmake_version < '3.1.0':
+        #         raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        # cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+        #               '-DPYTHON_EXECUTABLE=' + sys.executable]
+        cmake_args = [
+
+            # '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+            '-DUIPC_PYTHON_EXECUTABLE_PATH=' + sys.executable
+        ]
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
+                cfg.upper(),
+                extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['-j8']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get('CXXFLAGS', ''),
+            self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
+                              cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args,
+                              cwd=self.build_temp)
+
+# with (Path(__file__).resolve().parent / "README.md").open("r", encoding="utf-8") as f:
+#     long_description = f.read()
 
 # Installation operation
 setup(
@@ -32,8 +108,9 @@ setup(
     classifiers=[
         "Natural Language :: English",
         "Programming Language :: Python :: 3.10",
-        "Isaac Sim :: 2023.1.1",
         "Isaac Sim :: 4.5.0",
     ],
+    ext_modules=[CMakeExtension("uipc")],
+    cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
 )
